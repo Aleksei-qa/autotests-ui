90fbead9fd13641bb5b6fbe0d1aea854f93fcb5e
Aleksei-qa <aleksei.k91@mail.ru>
2026-03-23 09:59:02 +0300

    Работа с авторизацией в Playwright

--- playwright_registration_save_value.py
+++ playwright_registration_save_value.py
@@ -28,7 +28,7 @@ with sync_playwright() as playwright:
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-stage.json')
+    context = browser.new_context(storage_state='browser-stage.json') # Указываем файл с сохраненным состоянием
     page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
